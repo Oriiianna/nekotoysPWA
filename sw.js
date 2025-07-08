a8e61cd52b4be93ba3e98505923d5275166e2e9b
Orianna <122849804+Oriiianna@users.noreply.github.com>
2025-07-08 09:53:12 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -47,22 +47,26 @@ self.addEventListener('fetch', event => {
     if (event.request.method !== 'GET') return;
 
     event.respondWith(
-        fetch(event.request)
-            .then(response => {
-                if (response && response.status === 200 && response.type === 'basic') {
-                    const responseClone = response.clone();
-                    caches.open(CACHE_NAME)
-                        .then(cache => {
-                            cache.put(event.request, responseClone);
-                        });
-                }
-                return response;
-            })
-            .catch(() => {
-                if (event.request.mode === 'navigate') {
-                    return caches.match(BASE_PATH + 'offline.html');
-                }
-                return caches.match(event.request);
+        caches.match(event.request)
+            .then(cached => {
+                if (cached) return cached;
+                return fetch(event.request)
+                    .then(response => {
+                        if (
+                            response.status === 200 &&
+                            response.type === 'basic'
+                        ) {
+                            const responseClone = response.clone();
+                            caches.open(CACHE_NAME)
+                                .then(cache => cache.put(event.request, responseClone));
+                        }
+                        return response;
+                    })
+                    .catch(() => {
+                        if (event.request.mode === 'navigate') {
+                            return caches.match(BASE_PATH + 'offline.html');
+                        }
+                    });
             })
     );
 });
